00612660251cf220dac5aebff8666cc1662e7e25
Martin Giachetti <martin.giachetti@gmail.com>
2018-07-02 15:09:03 -0300

    Release new version

---
+++ preload/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm run distAll && \
+  cp "./dist/Mundial TV 1.0.0.exe" "./releases/Mundial TV.exe" && \
+  cp "./dist/Mundial TV 1.0.0.dmg" "./releases/Mundial TV.dmg" && \
+  git add . && \
+  git commit -m "Release new version" && \
+  git tag -f latest && \
+  git push origin master --tags
+
+
+
+
+
+
+
+
