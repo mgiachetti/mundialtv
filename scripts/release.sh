86c13d4047ab330bdc6d48312c25e4be6f9e3c79
Martin Giachetti <martin.giachetti@gmail.com>
2018-07-03 12:28:40 -0300

    Release new version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,11 @@
 
 npm run distAll && \
   cp "./dist/Mundial TV 1.0.0.exe" "./releases/Mundial TV.exe" && \
-  cp "./dist/Mundial TV 1.0.0.dmg" "./releases/Mundial TV.dmg" && \
+  cp "./dist/Mundial TV-1.0.0.dmg" "./releases/Mundial TV.dmg" && \
   git add . && \
   git commit -m "Release new version" && \
   git tag -f latest && \
-  git push origin master --tags
+  git push origin master --tags -f
 
 
 
